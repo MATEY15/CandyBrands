40dde647480cb02153eb373d29a5994dfc402216
matwey-project <act1987@mail.ru>
2023-08-08 02:03:38 +0300

    Add popup video, Error message, Start responsive

--- src/blocks/video-preview/video-preview.js
+++ src/blocks/video-preview/video-preview.js
@@ -1,4 +1,30 @@
 require("jquery.marquee");
+require("magnific-popup");
+
+$('.popup, .popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'inline',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+});
+
+$('.video-popup').magnificPopup({
+    type: 'inline',
+    callbacks: {
+        open: function () {
+            $('html').css('margin-right', 0);
+
+            $(this.content).find('video')[0].play();
+        },
+        close: function () {
+            $(this.content).find('video')[0].load();
+        }
+    }
+});
+
 function marqueeCreate() {
     const marqueeWrapper = document.querySelector('.video-preview__marquee');
 
